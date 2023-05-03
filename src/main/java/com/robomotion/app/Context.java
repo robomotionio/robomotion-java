4e7dfce6973fd08c217d171a60f267c603567b49
Hasan Gürsoy <hasan@robomotion.io>
2023-05-03 13:33:10 +0300

    context interface

--- src/main/java/com/robomotion/app/Context.java
+++ src/main/java/com/robomotion/app/Context.java
@@ -2,7 +2,14 @@ package com.robomotion.app;
 
 public interface Context {
 	public String GetID();
+
 	public void Set(String key, Object value);
+
 	public <T> T Get(String key, Class<T> cls);
+
 	public byte[] GetRaw();
+
+	public void SetRaw(byte[] data);
+
+	public boolean IsEmpty();
 }
